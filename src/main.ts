2840516c4541c5f2f6fea5ca6cc6a9470b65011e
Ethan Phelps <ethanrphelpsapps@gmail.com>
2024-04-14 20:10:36 -0400

    initial drag + drop / connection changes

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { Render } from "./render";
+
+const SIZE = 500;
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+      <canvas
+        id="treeCanvas"
+        class="canvas"
+        width=${SIZE}
+        height=${SIZE}
+      ></canvas>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const canvas = document.getElementById("treeCanvas") as HTMLCanvasElement;
+canvas.style.background = "#242424"
+
+const render = new Render();
